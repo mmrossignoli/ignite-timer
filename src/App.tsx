d5d1716de1516a1740a4d333cfece35350420193
Marcelo Rossignoli <mattosrossignoli@gmail.com>
2022-07-09 20:22:05 -0300

    router movido para pasta src

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'styled-components'
-import { Router } from './components/Router'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { BrowserRouter } from 'react-router-dom'
